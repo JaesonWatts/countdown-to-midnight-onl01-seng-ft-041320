e1c29c6ec86bbeabb76f7fdf0be05102e2a05b7e
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-14 09:18:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,11 +4,15 @@ def countdown(number)
   while number > 0 
     puts "#{number} SECOND(S)!"
     number -= 1
-    sleep(0.5) 
   end
   "HAPPY NEW YEAR!"
 end
 
 countdown_with_sleep(number)
-
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(0.5) 
+  end
+  "HAPPY NEW YEAR!"
 end
